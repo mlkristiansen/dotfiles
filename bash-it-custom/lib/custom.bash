a58e814320070d71d99be67dac6dfb3d52819e6a
Morten Kristiansen <github@mlk.dk>
2021-03-10 09:56:18 +0100

    Adds more memory to Grails

--- bash-it-custom/lib/custom.bash
+++ bash-it-custom/lib/custom.bash
@@ -3,7 +3,7 @@
 ## COPY
 # cp -v ~/.dotfiles/bash-it-custom/lib/custom.bash ~/.bash_it/lib/
 
-export GRAILS_OPTS="-Xmx2G -Xms512m -XX:MaxPermSize=512m -Dfile.encoding=UTF-8 -Djava.encoding=UTF-8"
+export GRAILS_OPTS="-Xmx5G -Xms512m -XX:MaxPermSize=512m -Dfile.encoding=UTF-8 -Djava.encoding=UTF-8"
 
 export LC_ALL=da_DK.UTF-8
 
